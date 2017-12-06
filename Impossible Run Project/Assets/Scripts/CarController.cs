4fa9d8e39c0891112d41505866faed77ef3fbe28
Sergio Cifuentes <cifupetit@gmail.com>
2017-12-06 11:12:06 +0100

    Config/Comprobar WheelColliders y definición e inicialización atributos CarController
    Se comprueba y se actualizan los atributos de los WheelColliders para hacer el coche más realista. Y se crea el Controller (script en c#) del Vehículo (CarController) y se intetnan definir e inicializar todos sus atributos.

---
+++ Impossible Run Project/Assets/Scripts/CarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    public WheelCollider ruedaDelDer;
+    public WheelCollider ruedaDelIzq;
+    public WheelCollider ruedaTraDer;
+    public WheelCollider ruedaTraIzq;
+
+    public float giroRuedas;
+    public float maxGiroRuedas;
+
+    public float fuerzaMotor;
+    public float maxFuerzaMotor;
+
+    public float freno;
+    public bool estoyFrenando;
+
+    public Vector3 posCentroDeMasas;    //posición del centro de masas del vehículo
+    public Vector3 posicionRueda;   //tanto posición como rotación servirán para posicionar el
+    public Quaternion rotacionRueda;//GameObject que posee el WheelCollider en el centro del modelo de rueda
+
+	// Use this for initialization
+	void Start () {
+        this.giroRuedas = 0.0f;
+        this.maxGiroRuedas = 25.0f;
+        this.fuerzaMotor = 0.0f;
+        this.maxFuerzaMotor = 800.0f;
+        this.estoyFrenando = false;
+        this.posCentroDeMasas = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
